6972ecac7e6db1a7e1b15de51364ba2635c33f3b
James Ambrose <galaxy2198@gmail.com>
2017-01-10 22:20:03 -0600

    Add files via upload

---
+++ TempConversions.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class TempConversions {
+
+
+public : 
+
+	double fahToCel(double Fahrenheit);
+	double celToFah(double Celsius);
+	double fahToKelvin(double Fahrenheit);
+	double kelvinToFah(double kelvin);
+	double celToKelvin(double Celsius);
+	double kelvinToCel(double kelvin);
+
+
+};
